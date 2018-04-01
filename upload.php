980155160a1a48777a3d6e19ff5075f83d2cc66b
bondarchukd <dbondarchuk@gmail.com>
2018-04-01 20:32:47 +0300

    1
    1

--- upload.php
+++ upload.php
@@ -1,26 +1,16 @@
 <?php
 
 if(isset($_POST["submit"])) {
-    // $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    // $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
-    // if($check !== false) {
-    //     echo "File is an image - " . $check["mime"] . ".";
-    //     $uploadOk = 1;
-    // } else {
-    //     echo "File is not an image.";
-    //     $uploadOk = 0;
-    // }
-}else{
-    die("No Image parse");
-}
 
+}else{
+    die("No file parse"); //needed fo stoping execude
+}
 
 $target_dir = "uploads/"; //specifies the directory where the file is going to be placed
 $uploadOk = 1;
 $unic = md5(uniqid(rand(), true));
 $target_file = $target_dir . $unic . basename($_FILES["fileToUpload"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // holds the file extension of the file (in lower case)
-// Check if image file is a actual image or fake image
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // holds the file extension of the file (in lower case)
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
@@ -29,15 +19,16 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 
 // Allow certain file formats
-if($imageFileType != "xlsx" && $imageFileType != "xls") {
+if($fileType != "xlsx" && $fileType != "xls") {
     $uploadOk = 0;
     die("Sorry, only xlsx and xls files are allowed.");
 }else{
     echo "All OK";
 }
 
+// check moving uploaded file to direction
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    //если файл загрузился в нужную папку,то
+    //если файл загрузился в нужную папку,то код идет дальше
     require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 
     $result = array();
@@ -49,6 +40,5 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     $result = $objPHPExcel->getActiveSheet()->toArray(); // выгружаем данные из объекта в массив
     print_r($result[1]);
 }
-
 
 ?>
