77a534b9cc6b83e85600811adc69a677cf3cae97
bondarchukd <dbondarchuk@gmail.com>
2018-04-03 06:58:31 +0300

    1
    1

--- upload.php
+++ upload.php
@@ -4,7 +4,8 @@
 // https://wp-kama.ru/question/kak-schitat-excel-fayl-php-skriptom
 
 // https://habrahabr.ru/post/140352/
-// https://stackoverflow.com/questions/11617077/looping-through-worksheets-with-phpexcel - HOW TO SWITCH WORKSHEET
+
+// https://gist.github.com/searbe/3284011
 
 if(isset($_POST["submit"])) {
 
@@ -15,6 +16,7 @@ if(isset($_POST["submit"])) {
 $target_dir = "uploads/"; //specifies the directory where the file is going to be placed
 
 $uploadOk = 1;
+
 $unic = md5(uniqid(rand(), true)); //generate random value for name of file
 
 $target_file = $target_dir . $unic . basename($_FILES["fileToUpload"]["name"]); // create unical name of file
@@ -40,24 +42,37 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     
     require_once dirname(__FILE__) . '/Classes/PHPExcel.php'; //if file uploaded to needed direction go on 
     
-    $result = array();
+    // $result = array();
     
     $file_type = PHPExcel_IOFactory::identify($target_file); // get type of file (xls, xlsx - needed fo correct proceeding
     
     $objReader = PHPExcel_IOFactory::createReader( $file_type ); // create the object for reading
     
     $objPHPExcel = $objReader->load($target_file); // upload data from the file to the object
-    
-    $result = $objPHPExcel->getActiveSheet()->toArray(); // download data from the object to the array
+
+//   $result = $objPHPExcel->getActiveSheet()->toArray(); // download data from the object to the array
+   // print_r($result);
+
+    // $result = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(1, 2)->getValue();
     // print_r($result);
+
+    //download data from the object to the array which consists of data from all sheets
+    foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+        $lists[] = $worksheet->toArray();
+    }
+
+    foreach($lists as $list){
+ echo '<table border="1">';
+ // loop rows
+ foreach($list as $row){
+   echo '<tr>';
+   // loop columns
+   foreach($row as $col){
+     echo '<td>'.$col.'</td>';
+ }
+ echo '</tr>';
+ }
+ echo '</table>';
 }
-echo "<table>";
-foreach ($result as $row) {
-   echo "<tr>";
-   foreach ($row as $column) {
-      echo "<td>$column</td>";
-   }
-   echo "</tr>";
-}    
-echo "</table>";
+}
 ?>
